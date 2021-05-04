1777af348f76196b031764ddbdcfff9a04989c38
mahdi <mahdielmaximo@gmail.com>
2021-05-04 15:49:30 +0430

    PushAfter and PushBefore functions added, plus an extra print function

--- cl.cpp
+++ cl.cpp
@@ -56,10 +56,148 @@ class Circular_linked_list{
 		}
 		void pop();
 		void pop(int data);
-		void push_after(int new_data,int pa_data);
-		void push_befor(int new_data,int pb_data);
+
+		//PushBefore function added by Mahdi Shirazi (GitHub ID: mhdshrz)
+		void PushAfter (int newdata, int PushAfterdata) {
+
+            node* ptr=nullptr;
+            node* tempo=nullptr;
+            ptr=this->current;
+
+            if(this->size==0) {
+
+                cout << "Can't be done!\n";
+
+                return;
+
+            }
+
+            while(ptr->getData()!=PushAfterdata) {
+
+                ptr=ptr->getNext();
+
+                if(ptr==this->current) break;
+
+            }
+
+            if(ptr==this->current) {
+
+                if(ptr->getData()!=PushAfterdata) {
+
+                    cout << "Not found!\n";
+
+                    return;
+
+                }
+
+            }
+
+            tempo=this->current;
+            this->current=ptr->getNext();
+
+            this->push(newdata);
+
+            //this->current=tempo;
+
+            return;
+
+        }
+
+		//PushAfter function added by Mahdi Shirazi (GitHub ID: mhdshrz)
+		void PushBefore (int newdata, int PushBeforedata) {
+
+            node* ptr=nullptr;
+            node* tempo=nullptr;
+            ptr=this->current;
+
+            if(this->size==0) {
+
+                cout << "Can't be done!\n";
+
+                return;
+
+            }
+
+            while(ptr->getData()!=PushBeforedata) {
+
+                ptr=ptr->getNext();
+
+                if(ptr==this->current) break;
+
+            }
+
+            if(ptr==this->current) {
+
+                if(ptr->getData()!=PushBeforedata) {
+
+                    cout << "Not found!\n";
+
+                    return;
+
+                }
+
+            }
+
+            tempo=this->current;
+            this->current=ptr;
+
+            this->push(newdata);
+
+            //this->current=tempo;
+
+            return;
+
+        }
+
 		void print();
 
+		//One parametric print added by Mahdi Shirazi (GitHub ID: mhdshrz)
+		//An extra feature which works as a function and takes an integer number and finds it in our list ...
+		//... as a data, then prints the  whole list beginning from the node having that integer as its data part.
+		void print (int data) {
+
+            node* ptr=nullptr;
+            node* tempo=nullptr;
+            ptr=this->current;
+
+            if(this->size==0) {
+
+                cout << "Can't be done!\n";
+
+                return;
+
+            }
+
+            while(ptr->getData()!=data) {
+
+                ptr=ptr->getNext();
+
+                if(ptr==this->current) break;
+
+            }
+
+            if(ptr==this->current) {
+
+                if(ptr->getData()!=data) {
+
+                    cout << "Not found!\n";
+
+                    return;
+
+                }
+
+            }
+
+            tempo=this->current;
+            this->current=ptr;
+
+            this->print();
+
+            this->current=tempo;
+
+            return;
+
+        }
 
 };
 
